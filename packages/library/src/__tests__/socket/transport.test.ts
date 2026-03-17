3c9f7a5e644f7323c4b78fdd62413c040a83c063
Lucio D'Alessandro <lda@juxt.pro>
2026-03-17 23:16:13 +0100

    Fix type

--- packages/library/src/__tests__/socket/transport.test.ts
+++ packages/library/src/__tests__/socket/transport.test.ts
@@ -7,7 +7,7 @@ class MockWebSocket {
 	static instances: MockWebSocket[] = [];
 	url: string;
 	protocols?: string | string[];
-	readyState = WebSocket.CONNECTING;
+	readyState: number = WebSocket.CONNECTING;
 	onopen: ((event: Event) => void) | null = null;
 	onclose: ((event: CloseEvent) => void) | null = null;
 	onmessage: ((event: MessageEvent) => void) | null = null;
